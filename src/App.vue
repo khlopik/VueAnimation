ce7af51510aa27cc1fd8df344628a0b371113a99
Oleksii Khlopotov <khlopik@gmail.com>
2017-12-05 17:59:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <select v-model="alertAnimation" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <br>
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <br><br>
+                <transition :name="alertAnimation">
+                    <div class="alert alert-info" v-if="show">This is some Info</div>
+                </transition>
+                <transition name="slide" type="animation" appear>
+                    <div class="alert alert-info" v-if="show">This is some Info</div>
+                </transition>
+                <transition
+                        enter-class=""
+                        enter-active-class="animated bounce"
+                        leave-class=""
+                        leave-active-class="animated shake"
+                >
+                    <div class="alert alert-info" v-if="show">This is some Info</div>
+                </transition>
+                <transition :name="alertAnimation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">This is some Info</div>
+                    <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
+                </transition>
+                <hr>
+                <button class="btn btn-primary" @click = "load = !load">Load / Remove Element</button>
+                <br><br>
+                <transition
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled">
+                    <div style="width: 100px; height: 100px; background-color: lightgreen" v-if="load"></div>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        show: false,
+        load: true,
+        alertAnimation: 'fade',
+      }
+    },
+    methods: {
+      beforeEnter(el) {
+        console.log('beforeEnter');
+      },
+      enter(el, done) {
+        console.log('enter');
+        done();
+      },
+      afterEnter(el) {
+        console.log('afterEnter');
+      },
+      enterCancelled(el) {
+        console.log('enterCancelled');
+      },
+      beforeLeave(el) {
+        console.log('beforeLeave');
+      }
+    }
+  }
+</script>
+
+<style>
+    .fade-enter {
+        opacity: 0;
+    }
+
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+
+    .fade-leave {
+        /*opacity: 1;*/
+    }
+
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    .slide-enter {
+        opacity: 0;
+        /*transform: translateY(20px);*/
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+        transition: opacity .5s;
+    }
+
+    .slide-leave {
+
+    }
+
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 1s;
+        opacity: 0;
+    }
+    
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
+    }
+</style>
